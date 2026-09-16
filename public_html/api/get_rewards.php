d099fcd609d2463d63da686a5c1a78efbf45414f
abidinsup <abidin1190@gmail.com>
2026-09-16 12:25:19 +0700

    Update rewards to use real photos instead of icons

--- public_html/api/get_rewards.php
+++ public_html/api/get_rewards.php
@@ -43,35 +43,35 @@ try {
             'id' => 1,
             'name' => 'Uang Tunai (Rp 150.000)',
             'points_required' => 3,
-            'image' => 'https://img.icons8.com/color/512/money-bag.png',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/4/4e/IDR_100000_2016_Series.jpg',
             'description' => 'Bonus uang tunai langsung ke rekening Anda'
         ],
         [
             'id' => 2,
             'name' => 'Kipas Angin / Magic Com',
             'points_required' => 6,
-            'image' => 'https://img.icons8.com/color/512/fan.png',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Desk_fan.jpg/800px-Desk_fan.jpg',
             'description' => 'Peralatan rumah tangga pilihan'
         ],
         [
             'id' => 3,
             'name' => 'Smart TV',
             'points_required' => 30,
-            'image' => 'https://img.icons8.com/color/512/tv.png',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/LG_Smart_TV.jpg/800px-LG_Smart_TV.jpg',
             'description' => 'Smart TV 32 Inch'
         ],
         [
             'id' => 4,
             'name' => 'Sepeda Motor',
             'points_required' => 360,
-            'image' => 'https://img.icons8.com/color/512/motorcycle.png',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Honda_Vario_150_eSP.jpg/800px-Honda_Vario_150_eSP.jpg',
             'description' => 'Satu unit sepeda motor'
         ],
         [
             'id' => 5,
             'name' => 'Paket Umroh',
             'points_required' => 650,
-            'image' => 'https://img.icons8.com/color/512/kaaba.png',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Kaaba_Masjid_Haraam_Makkah.jpg/800px-Kaaba_Masjid_Haraam_Makkah.jpg',
             'description' => 'Perjalanan Umroh eksklusif'
         ]
     ];
