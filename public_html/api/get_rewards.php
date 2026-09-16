354b398a39f680d9bfee082a149d7b9b300b54c4
abidinsup <abidin1190@gmail.com>
2026-09-16 13:09:21 +0700

    Revert to using relevant icons instead of photos

--- public_html/api/get_rewards.php
+++ public_html/api/get_rewards.php
@@ -43,35 +43,35 @@ try {
             'id' => 1,
             'name' => 'Uang Tunai (Rp 150.000)',
             'points_required' => 3,
-            'image' => 'https://images.unsplash.com/photo-1580519542036-ed47f3e42d17?w=600&h=400&fit=crop',
+            'image' => 'https://img.icons8.com/color/512/money-bag.png',
             'description' => 'Bonus uang tunai langsung ke rekening Anda'
         ],
         [
             'id' => 2,
             'name' => 'Kipas Angin / Magic Com',
             'points_required' => 6,
-            'image' => 'https://images.unsplash.com/photo-1618506469810-282bb698d822?w=600&h=400&fit=crop',
+            'image' => 'https://img.icons8.com/color/512/fan.png',
             'description' => 'Peralatan rumah tangga pilihan'
         ],
         [
             'id' => 3,
             'name' => 'Smart TV',
             'points_required' => 30,
-            'image' => 'https://images.unsplash.com/photo-1593359677879-a4bb92f829d1?w=600&h=400&fit=crop',
+            'image' => 'https://img.icons8.com/color/512/tv.png',
             'description' => 'Smart TV 32 Inch'
         ],
         [
             'id' => 4,
             'name' => 'Sepeda Motor',
             'points_required' => 360,
-            'image' => 'https://images.unsplash.com/photo-1558981403-c5f9899a28bc?w=600&h=400&fit=crop',
+            'image' => 'https://img.icons8.com/color/512/motorcycle.png',
             'description' => 'Satu unit sepeda motor'
         ],
         [
             'id' => 5,
             'name' => 'Paket Umroh',
             'points_required' => 650,
-            'image' => 'https://images.unsplash.com/photo-1565552643952-b43cb314f4fa?w=600&h=400&fit=crop',
+            'image' => 'https://img.icons8.com/color/512/kaaba.png',
             'description' => 'Perjalanan Umroh eksklusif'
         ]
     ];
